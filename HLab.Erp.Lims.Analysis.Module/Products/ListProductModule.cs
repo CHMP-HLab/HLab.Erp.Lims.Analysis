505788b9ae1888c32094b3faa3ebb2956f094775
mathieu <mathieu@lesbouys.net>
2020-02-13 20:02:02 +0100

    Products

--- HLab.Erp.Lims.Analysis.Module/Products/ListProductModule.cs
+++ HLab.Erp.Lims.Analysis.Module/Products/ListProductModule.cs
@@ -4,32 +4,17 @@ using System.Text;
 using System.Windows.Input;
 using HLab.Core.Annotations;
 using HLab.DependencyInjection.Annotations;
+using HLab.Erp.Base.Wpf.Entities;
+using HLab.Erp.Base.Wpf.Entities.Customers;
 using HLab.Erp.Core;
 using HLab.Notify.PropertyChanged;
 
 namespace HLab.Erp.Lims.Analysis.Module.Products
 {
-    using H = NotifyHelper<ListProductModule>;
+    using H = NotifyHelper<ProductsDataModule>;
 
-    public class ListProductModule : IPostBootloader
+    public class ProductsDataModule : ErpDataModule<ProductsDataModule,ListProductViewModel>
     {
-        private readonly IErpServices _erp;
 
-        [Import] public ListProductModule(IErpServices erp)
-        {
-            _erp = erp;
-            H.Initialize(this);
-        }
-        public ICommand OpenListProductCommand { get; } = H.Command(c => c.Action(
-            e => e._erp.Docs.OpenDocumentAsync(typeof(ListProductViewModel))
-        ));
-
-        public void Load()
-        {
-            _erp.Menu.RegisterMenu("data", "products", "{Products}",
-                OpenListProductCommand,
-                "icons/sample/drugs");
-
-        }
     }
 }
