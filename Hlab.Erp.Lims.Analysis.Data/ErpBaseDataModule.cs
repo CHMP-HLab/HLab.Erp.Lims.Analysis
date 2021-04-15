855dc2c5674f628de199bb354f09649c70b0ca33
mgth <mathieu@mgth.fr>
2021-04-15 15:03:59 +0200

    Data migration implementation

---
+++ Hlab.Erp.Lims.Analysis.Data/ErpBaseDataModule.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using HLab.Core.Annotations;
+using HLab.DependencyInjection.Annotations;
+using HLab.Erp.Base.Data;
+using HLab.Erp.Data;
+using Npgsql;
+using Org.BouncyCastle.Crypto.Engines;
+
+namespace HLab.Erp.Lims.Analysis.Data
+{
+    [Export(typeof(IBootloader))]
+    public class HLabErpLimsAnalysisDataUpdaterModule : DataUpdaterModule
+    {
+        public HLabErpLimsAnalysisDataUpdaterModule()
+        { }
+
+        protected override ISqlBuilder GetSqlUpdater(string version, ISqlBuilder builder)
+        {
+            switch (version)
+            {
+                case "0.0.0.0":
+                    builder
+                        .Table<Form>()
+                            .AddColumn(f => f.IconPath)
+                        .Table<LinkedDocument>()
+                            .Create()
+                        .Table<AnalysisMotivation>()
+                            .Create()
+                        .Table<Sample>()
+                            .AddColumn(s => s.AnalysisMotivation)
+                            .RenameColumn("State", s => s.ConformityId)
+                        .Table<SampleTestResult>()
+                            .RenameColumn("StateId", s => s.ConformityId)
+                        .Table<Pharmacopoeia>()
+                            .RenameColumn("NameEn", s => s.Name)
+                            .AddColumn(p => p.IconPath)
+                        .Table<Icon>()
+                            .AddColumn(i => i.Foreground)
+                        .Version("2.0.0.0")
+                        ;
+                    break;
+                case "2.0.0.0":
+                    builder
+                        .Table<TestClassUnitTest>()
+                            .AddColumn(t => t.TestClass)
+                            .RenameColumn("AssayName", t=> t.TestName)
+                            .RenameColumn("Specifications", t=> t.Specification)
+                            .AddColumn(t=> t.Name)
+                            .AddColumn(t=> t.SpecificationValues)
+                            .AddColumn(t=> t.SpecificationDone)
+                            .AddColumn(t=> t.ConformityId)
+                            .AddColumn(t=> t.Conformity)
+                            .AddColumn(t=> t.MandatoryDone)
+                        .Table<SampleForm>()
+                            .RenameColumn("SpecificationsDone", t=> t.SpecificationDone)
+                            .RenameColumn("SpecValues", t=> t.SpecificationValues)
+                            .RenameColumn("Values", t=> t.ResultValues)
+                        .Table<FormClass>()
+                            .AddColumn(t=> t.CodeHash)
+                        .Table<Continent>()
+                            .AddColumn(c => c.Code)
+                        .Version("2.1.0.0");
+                    break;
+                case "2.1.0.0":
+                    builder
+                        .Table<SampleTest>()
+                        .RenameColumn("SpecificationsDone", t => t.SpecificationDone)
+
+                        .Table<SampleForm>()
+                        .RenameColumn("State", s => s.ConformityId);
+                    break;
+
+            }
+            return base.GetSqlUpdater(version, builder);
+        }
+    }
+}
---
+++ Hlab.Erp.Lims.Analysis.Data/SQL/Update-From-0.0.0.0.sql
@@ -0,0 +1,25 @@
+﻿DELETE FROM public."SampleForm" WHERE "SampleId" < 0;
+
+UPDATE public."Pharmacopoeia" SET
+"Name" = '{International Pharmacopeia}'::text WHERE
+"Id" = 1;
+UPDATE public."Pharmacopoeia" SET
+"Name" = '{European Pharmacopoeia}'::text WHERE
+"Id" = 2;
+UPDATE public."Pharmacopoeia" SET
+"Name" = '{United States Pharmacopeia}'::text WHERE
+"Id" = 3;
+UPDATE public."Pharmacopoeia" SET
+"Name" = '{British Pharmacopoeia}'::text WHERE
+"Id" = 4;
+UPDATE public."Pharmacopoeia" SET
+"Name" = '{Internal Method}'::text WHERE
+"Id" = 5;
+INSERT INTO public."Pharmacopoeia" (
+"Name", "Id") VALUES (
+'{Manufaturer Method}'::text, '6'::integer)
+ returning "Id";
+
+UPDATE "Icon" SET "Foreground" = -16777216;
+UPDATE "Icon" SET "Foreground" = null WHERE "Path" Contains "Flag";
+
